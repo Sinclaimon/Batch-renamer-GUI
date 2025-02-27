38cfe1da0b55d473a7314291978672d58e53cbda
Sinclaimon <simi.matousek@gmail.com>
2025-02-27 19:23:00 +0000

    renamed GUI script, formatting

--- Batch-renamer-GUI/batch_renamer_gui.py
+++ Batch-renamer-GUI/batch_renamer_gui.py
@@ -2,10 +2,7 @@ import sys
 import os
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import *
-# You'll need to make this ui in QtDesigner
-# And convert it to a .py file using the MakeUIPy.bat file
 from batch_renamer_ui import Ui_MainWindow 
-# Recommend you rename this
 import batch_renamer_lib
 
 class BatchRenamerWindow(QMainWindow, Ui_MainWindow):
@@ -36,22 +33,33 @@ class BatchRenamerWindow(QMainWindow, Ui_MainWindow):
         """
         Add tooltips to UI elements
         """
-        self.FilePathEdit.setToolTip("Enter the file path or browse to select a folder")
-        self.BrowseButton.setToolTip("Browse to select a folder")
-        self.FiletypesEdit.setToolTip("Enter the file types to include (e.g., .txt, .png)")
-        self.PrefixEdit.setToolTip("Enter the prefix to add to file names")
-        self.SufixEdit.setToolTip("Enter the suffix to add to file names")
-        self.StringsToFindEdit.setToolTip("Enter the strings to find, separated by commas")
-        self.StringsToReplaceEdit.setToolTip("Enter the string to replace the found strings")
-        self.CopyRadioButton.setToolTip("Check to copy files instead of moving them")
-        self.RunButton.setToolTip("Run the batch renamer")
+        self.FilePathEdit.setToolTip(
+            "Enter the file path or browse to select a folder")
+        self.BrowseButton.setToolTip(
+            "Browse to select a folder")
+        self.FiletypesEdit.setToolTip(
+            "Enter the file types to include (e.g., .txt, .png)")
+        self.PrefixEdit.setToolTip(
+            "Enter the prefix to add to file names")
+        self.SufixEdit.setToolTip(
+            "Enter the suffix to add to file names")
+        self.StringsToFindEdit.setToolTip(
+            "Enter the strings to find, separated by commas")
+        self.StringsToReplaceEdit.setToolTip(
+            "Enter the string to replace the found strings")
+        self.CopyRadioButton.setToolTip(
+            "Check to copy files instead of moving them")
+        self.RenameRadioButton.setToolTip(
+            "Check to rename files")
+        self.RunButton.setToolTip(
+            "Run the batch renamer")
 
     def get_filepath(self):
         """
         Open a file dialog for browsing to a folder
         """
+        self.logger.info("Browse button clicked")
         try:
-            self.logger.info("Browse button clicked")
             self.filepath = QFileDialog().getExistingDirectory()
             self.logger.info(f"Selected filepath: {self.filepath}")
             self.set_filepath()
@@ -62,8 +70,8 @@ class BatchRenamerWindow(QMainWindow, Ui_MainWindow):
         """
         Set lineEdit text for filepath
         """
+        self.logger.info("Setting filepath in FilePathEdit")
         try:
-            self.logger.info("Setting filepath in FilePathEdit")
             self.FilePathEdit.setText(self.filepath)
             self.update_list()
         except Exception as e:
@@ -99,7 +107,8 @@ class BatchRenamerWindow(QMainWindow, Ui_MainWindow):
             prefix = self.PrefixEdit.text()
             suffix = self.SufixEdit.text()
 
-            self.logger.info("Running batch renamer with the following parameters:")
+            self.logger.info(
+                "Running batch renamer with the following parameters:")
             self.logger.info(f"Filepath: {filepath}")
             self.logger.info(f"Copy files: {copy_files}")
             self.logger.info(f"Filetypes: {filetypes}")
@@ -116,7 +125,13 @@ class BatchRenamerWindow(QMainWindow, Ui_MainWindow):
             self.batch_renamer.prefix = prefix
             self.batch_renamer.suffix = suffix
 
-            self.batch_renamer.rename_files_in_folder(filepath, filetypes, strings_to_find, string_to_replace, prefix, suffix, copy_files)
+            self.batch_renamer.rename_files_in_folder(filepath, 
+                                                      filetypes, 
+                                                      strings_to_find, 
+                                                      string_to_replace, 
+                                                      prefix, 
+                                                      suffix, 
+                                                      copy_files)
 
             # Update the file list after renaming
             self.update_list()
