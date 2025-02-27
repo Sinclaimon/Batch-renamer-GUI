8047c4f256a9a1dda1df1e5f1df3c5227ec57b50
Sinclaimon <simi.matousek@gmail.com>
2025-02-27 14:28:50 +0000

    reorganised the project files

--- MakeUIPy.bat
+++ MakeUIPy.bat
@@ -1,1 +1,6 @@
-python -m PyQt6.uic.pyuic -x batch_renamer.ui -o batch_renamer_ui.py
+@echo off
+REM This batch file converts the UI file created in Qt Designer into a Python file.
+
+pyuic6 -x batch_renamer_ui.ui -o batch_renamer_ui.py
+
+echo Conversion complete. The UI file has been successfully converted to Python.
